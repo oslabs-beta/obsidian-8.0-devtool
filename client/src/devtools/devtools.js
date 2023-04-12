c4510d3166ad5455c1007c2298590c70be33e7fc
Joan Mariel Manto <joanmarielmanto@Joans-MacBook-Pro.local>
2023-04-11 19:17:31 -0700

    edited readme to be more specific and corrected links, deleted unneeded package.json, edited manifest.json and devtools.js to remove 8.0 in title
    Co-authored-by: David Norman <120237773+DavidMNorman@users.noreply.github.com>
    Co-authored-by: David Kim <david.toyou.kim@gmail.com>
    Co-authored-by: Eileen Cho <109772371+exlxxn@users.noreply.github.com>
    Co-authored-by: Joan Manto <120276510+JoanManto@users.noreply.github.com>

--- client/src/devtools/devtools.js
+++ client/src/devtools/devtools.js
@@ -1,7 +1,7 @@
 // This file will create our new Devtool Tab in Devtools
 //Learn more about chrome extension architecture here: https://developer.chrome.com/docs/extensions/mv3/architecture-overview/
 chrome.devtools.panels.create(
-  'Obsidian 8.0 DevTools',
+  'Obsidian Developer Tool',
   '../assets/logoSilver.jpg', 
   // pagePath destination is relative to the manifest.json file
   '../dist/index.html'
